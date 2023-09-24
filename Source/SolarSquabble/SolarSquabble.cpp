4101d56c08553ced3f15ea2c39f17a566823a224
Ken Crimmins <37313064+kencrim@users.noreply.github.com>
2023-09-23 20:15:03 -0700

    Bulk Initial Commit

---
+++ Source/SolarSquabble/SolarSquabble.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SolarSquabble.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SolarSquabble, "SolarSquabble" );
+
+DEFINE_LOG_CATEGORY(LogSolarSquabble)
+ 
